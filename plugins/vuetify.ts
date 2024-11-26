895e7472a2c4a9d7d4766dab8471c41ce27c8fb7
Lerich Osay <lerichyaso@gmail.com>
2024-11-26 13:36:54 -0600

    finally working calendar UI

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,10 +1,11 @@
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { VCalendar } from 'vuetify/labs/VCalendar'
 
 export default defineNuxtPlugin(nuxtApp => {
   const vuetify = createVuetify({
-    components,
+    components: {...components, VCalendar},
     directives,
   })
 
