978a9211b58c75d060c13a24b17636f7aa8965f4
Bilal Saiyed <bilalfsaiyed@gmail.com>
2025-03-06 16:28:02 -0600

    removed login.ts

--- server/utils/login.ts
+++
@@ -1,7 +0,0 @@
-import  crypto  from 'crypto';
-
-export const encryptPassword=(password:string, algorithm = 'sha256'): string =>{
-  const hash = crypto.createHash(algorithm);
-  hash.update(password);
-  return hash.digest('hex');
-}
