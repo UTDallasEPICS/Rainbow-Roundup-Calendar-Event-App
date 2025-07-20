878d8f95ba1847af9190ca038a881071f17c4f8a
Caleb Beeson <caleb.f.beeson@gmail.com>
2025-07-20 02:39:48 -0500

    Added email code, it's not working yet

--- server/api/event/index.post.ts
+++ server/api/event/index.post.ts
@@ -1,7 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 import { defineEventHandler, readBody, setResponseStatus } from "h3";
 import { getServerSession } from "#auth";
+import { createTransport } from "nodemailer";
 import type { User } from "../../../types/session";
+
+const config = useRuntimeConfig();  // Access config for smtp
 
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
@@ -40,6 +43,49 @@ export default defineEventHandler(async (event) => {
         currentCapacity: 0,
       },
     });
+
+    // Send email notification about the new event
+    const transport = createTransport({
+      host: config.smtpHost, 
+        port: Number(config.smtpPort),
+        secure: false,
+        auth: {
+          user: config.smtpUser,
+          pass: config.smtpPass,
+        },
+    });
+
+    const users = await prisma.user.findMany({
+      where: {
+        GlobalNotif: true,
+        emailVerified: true,
+        role: "USER",
+      },
+      select: {
+        email: true,
+      },
+    });
+
+    // REMOVE
+    console.log(users);
+    // REMOVE
+
+    for (const user of users) {
+      const mailOptions = {
+        to: user.email,
+        from: config.smtpFrom,
+        subject: "New Event from Rainbow Roundup",
+        text: "Hey, Rainbow Roundup is hosting a new event! You can check it out here <Add link later>",
+        html: "<p>Hey, <strong>Rainbow Roundup</strong> is hosting a new event! You can check it out here <strong>Add link later</strong></p>",
+      };
+
+      transport.sendMail(mailOptions, (err, info) => {
+        if (err) {
+          console.log(`Error sending email to ${user.email}:`, err);
+        }
+      });
+    }
+
     setResponseStatus(event, 200);
     return {
       data: newEvent,
