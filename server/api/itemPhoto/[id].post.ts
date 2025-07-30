10093a7ac0a72f71b7e822116e0b37262c263cac
Safah <safa.aman@hotmail.com>
2025-07-30 04:09:11 -0500

    post api done, needs testing

--- server/api/itemPhoto/[id].post.ts
+++ server/api/itemPhoto/[id].post.ts
@@ -1,13 +1,24 @@
-import { defineEventHandler, setResponseStatus, readBody } from "h3";
+import { defineEventHandler, setResponseStatus, readMultipartFormData } from "h3";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import type { User } from "../../../types/session";
 import { getServerSession } from "#auth";
+import { jobs } from "googleapis/build/src/apis/jobs";
+
+const config = useRuntimeConfig();
+const s3 = new S3Client({
+    region: config.AWS_REGION,
+    credentials: {
+            accessKeyId: config.NUXT_AWS_ACCESS_KEY_ID!,
+            secretAccessKey: config.NUXT_AWS_SECRET_ACCESS_KEY!,
+    },
+});
 
 export default defineEventHandler(async (event) => {
     const id = getRouterParam(event, 'id');
     const prisma = event.context.prisma;
     const session = await getServerSession(event);
     const user = session?.user as User | undefined;
-    const body = await readBody(event);
+    const form = await readMultipartFormData(event);
 
     if (!user?.role || (user.role !== "SUPER" && user.role !== "ADMIN")) {
         throw createError({
@@ -39,13 +50,43 @@ export default defineEventHandler(async (event) => {
             };
         }
 
+        // get file from form data
+        const file = form?.find((key) => key.name === "image");
 
+        if (!file || !file.data || !file.filename) {
+            setResponseStatus(event, 400);
+            return {
+                success: false,
+                error: "No file uploaded",
+            }
+        }
 
-        setResponseStatus(event, 201);
+        // upload file to s3 bucket
+        const objectKey = `itemPhotos/${Date.now}-${file.filename}`;
+        const bucketName = config.NUXT_AWS_S3_BUCKET_NAME!;
+        const region = config.AWS_REGION!;
+        const command = new PutObjectCommand({
+            Bucket: bucketName,
+            Key: objectKey,
+            Body: file.data,
+            ContentType: file.type,
+        });
+        s3.send(command);
+
+        // create record in table
+        const s3url = `https://${bucketName}.s3.${region}.amazonaws.com/${objectKey}`;
+        const itemPhoto = await prisma.itemPhoto.create({
+            data: {
+                url: s3url,
+                itemId: id,
+            }
+        });
+
+        setResponseStatus(event, 200);
         return {
             success: true,
-            itemPhoto: itemPhoto,
-        };
+            data: itemPhoto,
+        }
     } catch (error) {
         const errorMessage = error instanceof Error? error.message : "Unknown error occurred";
         setResponseStatus(event, 500);
