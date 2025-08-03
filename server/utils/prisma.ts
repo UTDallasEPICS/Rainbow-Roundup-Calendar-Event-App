7ec96e1fe96e17c10275973eed3c8b8442884195
Caleb Beeson <caleb.f.beeson@gmail.com>
2025-08-03 14:57:54 -0500

    fixed typo in server/utils/prisma.ts

--- server/utils/prisma.ts
+++ server/utils/prisma.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 
 // add this so typescript doesn't yell at me :(
-const globalForPrisma = globalThis as unkown as { prisma? : PrismaClient };
+const globalForPrisma = globalThis as unknown as { prisma? : PrismaClient };
 
 // creates prisma client if it doesn't exist
 export const prisma = globalForPrisma.prisma ?? new PrismaClient();
