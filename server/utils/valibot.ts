be79be63481f801a674a79258773aa92c4b1e303
Aaron Menezes <123612000+aaron1857@users.noreply.github.com>
2025-10-26 15:37:33 -0500

    create scheduler for native notifications

--- server/utils/valibot.ts
+++
@@ -1,8 +0,0 @@
-import * as v from "valibot";
-
-export const validate =
-  <TSchema extends v.GenericSchema>(schema: TSchema) =>
-  (input: unknown) =>
-    v.parse(schema, input);
-
-export { v };
