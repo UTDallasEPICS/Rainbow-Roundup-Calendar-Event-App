e33d84458decef4699ec129e474406fd75a94d77
Amelie Yu <amelie.m.yu@gmail.com>
2025-12-03 13:26:23 -0600

    progress for editing part

--- pages/order/[id].vue
+++ pages/order/[id].vue
@@ -34,8 +34,8 @@
                         <p>Order ID</p>
                         <p>{{ order.id }}</p>
                     </h1>
-                    <div v-if="isAdmin" class="text-right">
-                        <button class="bg-blue-300 p-2 rounded-md font-bold hover:bg-blue-400 cursor-pointer transition duration-150" @click="">
+                    <div v-if="isAdmin && !isEditing" class="text-right">
+                        <button class="bg-blue-300 p-2 rounded-md font-bold hover:bg-blue-400 cursor-pointer transition duration-150" @click="makeEdits()">
                             Edit
                         </button>
                     </div>
@@ -45,7 +45,7 @@
                     <!-- header color -->
                     <div class="h-8 bg-blue-300 rounded-t-lg"></div>
 
-                    <div class="grid grid-cols-1 md:grid-cols-2">
+                    <div v-if="!isEditing" class="grid grid-cols-1 md:grid-cols-2">
                         <!-- left side -->
                         <div class="col-span-1 flex flex-col gap-2 p-4">
                             <div>
@@ -130,7 +130,113 @@
                             </div>
                         </div>
                     </div>
+
+                    <!-- EDITS SECTION -->
+                    <div v-else class="grid grid-cols-1 md:grid-cols-2">
+                        <!-- left side -->
+                        <div class="col-span-1 flex flex-col gap-2 p-4">
+                            <div>
+                                <h2 class="text-lg font-bold">Customer Name</h2>
+                                <p>{{ order.User.firstname }} {{ order.User.lastname }}</p>
+                            </div>
+                            <div>
+                                <h2 class="text-lg font-bold">Contact Information</h2>
+                                <p>{{ order.User.email }}</p>
+                                <p v-if="order.User.phone != null && order.User.phone != ''">{{ order.User.phone }}</p>
+                            </div>
+                            <div>
+                                <h2 class="text-lg font-bold">Placed At</h2>
+                                <p>{{ formatDateTime(order.placedAt) }}</p>
+                            </div>
+                            <div>
+                                <label for="status" class="text-lg font-bold">Order Status</label>
+                                <!-- <p>{{ order.status }}</p> -->
+                                <div class="border border-gray-400 p-1 rounded">
+                                    <select name="status" v-model="editedOrder.status" class="w-full">
+                                        <option value="UNCONFIRMED">UNCONFIRMED</option>
+                                        <option value="CONFIRMED">CONFIRMED</option>
+                                        <option value="PAID">PAID</option>
+                                        <option value="DELIVERED">DELIVERED</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <!-- order list -->
+                            <div>
+                                <h2 class="text-lg font-bold">Items</h2>
+                                <div class="grid grid-cols-3">
+                                    <div class="col-span-1 font-bold">
+                                        Item
+                                    </div>
+                                    <div class="col-span-1 text-right font-bold">
+                                        Quantity
+                                    </div>
+                                    <div class="col-span-1 text-right font-bold">
+                                        Price
+                                    </div>
+                                </div>
+                                <div v-for="item in order.OrderItems" class="grid grid-cols-3">
+                                    <div class="col-span-1">
+                                        {{ item.ItemVariants.item.name }} - {{ item.ItemVariants.size }}
+                                    </div>
+                                    <div class="col-span-1 text-right">
+                                        {{ item.quantity }}
+                                    </div>
+                                    <div class="col-span-1 text-right">
+                                        $ {{ (item.ItemVariants.item.price * item.quantity).toFixed(2) }}
+                                    </div>
+                                </div>
+                                <div class="grid grid-cols-2 border-t-2">
+                                    <div class="col-span-1 font-bold">
+                                        Total
+                                    </div>
+                                    <div class="col-span-1 text-right">
+                                        $ {{ totalOrderPrice.toFixed(2) }}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- right side -->
+                        <div class="col-span-1 flex flex-col gap-2 p-4">
+                            <div>
+                                <h2 class="text-lg font-bold">Order Type</h2>
+                                <p>{{ order.orderType }}</p>
+                            </div>
+                            <!-- pickup -->
+                            <div v-if="order.orderType == 'PICKUP' || order.orderType == OrderType.PICKUP" class="flex flex-col gap-2">
+                                <div>
+                                    <h2 class="text-lg font-bold">Pickup At</h2>
+                                    <EventCard :event="order.event" />
+                                </div>
+                            </div>
+                            <!-- shipping -->
+                            <div v-else class="flex flex-col gap-2">
+                                <div>
+                                    <h2 class="text-lg font-bold">Shipping Address</h2>
+                                    <p>{{ order.shippingAddress }}</p>
+                                </div>
+                                <div>
+                                    <h2 class="text-lg font-bold">USPS Tracking Number</h2>
+                                    <p v-if="order.trackingNumber != null && order.trackingNumber != undefined">
+                                        {{ order.trackingNumber }}
+                                    </p>
+                                    <p v-else class="text-gray-400 text-sm font-normal">N/A</p>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- manage buttons -->
+                        <div class="col-span-2 flex justify-center gap-4 p-4">
+                            <button class="bg-lime-300 px-4 py-2 mx-1 rounded hover:bg-lime-400 cursor-pointer transition" @click="saveChanges()">
+                                Save
+                            </button>
+                            <button class="bg-gray-300 px-4 py-2 mx-1 rounded hover:bg-gray-400 cursor-pointer transition" @click="cancelEdit()">
+                                Revert
+                            </button>
+                        </div>
+                    </div>
+                    
                 </div>
+                
 
                 <div v-if="!isAdmin" class="text-gray-400 text-sm font-normal text-center mt-2">
                     Email info@rrup.org to make changes to your order.
@@ -142,9 +248,9 @@
 </template>
 
 <script setup>
-import { ref, computed } from 'vue'
+import { ref, computed, reactive } from 'vue'
 import { useRoute } from 'vue-router'
-import { OrderType } from '@prisma/client'
+import { OrderType, OrderStatus } from '@prisma/client'
 
 // Access the route and router
 const route = useRoute()
@@ -156,6 +262,20 @@ const { data: session } = await authClient.getSession();
 const order = ref(null)
 const isLoading = ref(true)
 const totalOrderPrice = ref(0)
+const isEditing = ref(false);
+
+const editedOrder = reactive({
+    id: null,
+    status: OrderStatus.UNCONFIRMED,
+    orderType: OrderType.PICKUP,
+    shippingAddress: null,
+    pickupEventID: null,
+    trackingNumber: 0,
+    placedAt: Date.now(),
+    updatedAt: Date.now(),
+})
+
+
 
 
 // Fetch user info from API
@@ -206,4 +326,31 @@ function calculateOrderPrice() {
    totalOrderPrice.value = price;
 
 }
+
+function makeEdits() {
+    setEditedOrderToOrder();
+    isEditing.value = true;
+}
+
+function saveChanges() {
+    // put changes
+    isEditing.value = false
+}
+
+function cancelEdit() {
+    setEditedOrderToOrder();
+    isEditing.value = false
+}
+
+function setEditedOrderToOrder() {
+    editedOrder.id = order.value.id
+    editedOrder.status = order.value.status
+    editedOrder.orderType = order.value.orderType
+    editedOrder.shippingAddress = order.value.shippingAddress
+    editedOrder.pickupEventID = order.value.pickupEventID
+    editedOrder.trackingNumber = order.value.trackingNumber
+    editedOrder.placedAt = new Date(order.value.placedAt)
+    editedOrder.updatedAt = new Date(order.value.updatedAt)
+}
+
 </script>
