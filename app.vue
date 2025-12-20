fbe183b69a5ff4256a4a0f14691a47f9f8685bb8
fegelsamuel <fegelsamuel@gmail.com>
2025-12-20 16:13:56 -0600

    SEND THAT SHIT TO BIKINI BOTTOM

--- app.vue
+++ app.vue
@@ -70,8 +70,8 @@
           </NuxtLink>
           <NuxtLink to="/merchandise" @click="navigate('Merchandise')" class="text-gray-700 hover:text-black">Merchandise
           </NuxtLink>
-          <a href="https://buy.stripe.com/test_14k6op0Et2oF9xKaEE" @click="navigate('Donate')"
-            class="text-gray-700 hover:text-black">Donate</a>
+          <a class="text-gray-700 hover:text-black" href="https://buy.stripe.com/test_14k6op0Et2oF9xKaEE" @click="navigate('Donate')">Donate
+          </a>
           <NuxtLink v-if="session" to="/profile" @click="navigate('Profile')" class="text-gray-700 hover:text-black">Profile
           </NuxtLink>
           <NuxtLink v-if="(!session)" to="/signup" @click="navigate('Sign Up')" class="text-gray-700 hover:text-black">
@@ -82,25 +82,6 @@
           <button @click="promptInstall" class="text-gray-700 hover:text-black">
             Install App
           </button>
-          <!-- 
-          <button @click="requestNotificationPermission" class="text-gray-700 hover:text-black"
-            aria-label="Toggle notifications">
-            <span v-if="isSubscribed">
-              Bell icon
-              <svg class="w-5 h-5 inline" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                  d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
-              </svg>
-            </span>
-            <span v-else>
-              Bell with slash
-              <svg class="w-5 h-5 inline" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24">
-                <path stroke-linecap="round" stroke-linejoin="round"
-                  d="M9.143 17.082a24.248 24.248 0 0 0 3.844.148m-3.844-.148a23.856 23.856 0 0 1-5.455-1.31 8.964 8.964 0 0 0 2.3-5.542m3.155 6.852a3 3 0 0 0 5.667 1.97m1.965-2.277L21 21m-4.225-4.225a23.81 23.81 0 0 0 3.536-1.003A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6.53 6.53m10.245 10.245L6.53 6.53M3 3l3.53 3.53" />
-              </svg>
-            </span>
-          </button>
-          -->
         </nav>
 
         <!-- Hamburger Button - md: sizing -->
@@ -136,12 +117,9 @@
           <a href="https://buy.stripe.com/test_14k6op0Et2oF9xKaEE"
             class="block py-2 text-gray-700 hover:text-black hover:bg-gray-50 rounded px-2"
             @click="handleMobileNavClick">Donate</a>
-          <NuxtLink v-if="session && !loadingAuth" to="/profile" class="block py-2 text-gray-700 hover:text-black hover:bg-gray-50 rounded px-2"
+          <NuxtLink v-if="session" to="/profile" class="block py-2 text-gray-700 hover:text-black hover:bg-gray-50 rounded px-2"
           @click.native="handleMobileNavClick">Profile</NuxtLink>
-          <NuxtLink v-if="loadingAuth" to="/"  class="block py-2 text-gray-700 hover:text-black hover:bg-gray-50 rounded px-2"
-            @click.native="handleMobileNavClick">
-            Loading Session</NuxtLink>
-          <NuxtLink v-else-if="(!session) && !loadingAuth" to="/signup" class="block py-2 text-gray-700 hover:text-black hover:bg-gray-50 rounded px-2"
+          <NuxtLink v-if="(!session)" to="/signup" class="block py-2 text-gray-700 hover:text-black hover:bg-gray-50 rounded px-2"
             @click.native="handleMobileNavClick">
             Sign Up/Log In</NuxtLink>
           <button v-else @click="logout(); handleMobileNavClick()"
@@ -273,14 +251,15 @@ const updateSubscriptionStatus = () => {
 
 notificationError.value = computed(() => {
   if (!($pwa?.getSWRegistration())) {
-    notificationError.value = "Not supported in your browser";
+    return "Not supported in your browser";
   }
   else {
     try {
       const user = session.value.user;
+      return null;
     }
     catch {
-      notificationError.value = "Please login to register notifications"; // 
+      return "Please login to register notifications";  
     }
   }
 })
