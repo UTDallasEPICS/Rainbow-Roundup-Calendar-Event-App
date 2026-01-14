5a71968bf9885487e54ec01e4ac95ca3c3b30726
Aaron Menezes <123612000+aaron1857@users.noreply.github.com>
2026-01-14 13:16:23 -0600

    fix condition checking for notification unsubscription

--- app.vue
+++ app.vue
@@ -315,16 +315,17 @@ notificationError.value = computed(() => {
   }
 })
 const requestNotificationPermission = () => {
-  if(isSubscribedToPush){
+  if(isSubscribedToPush.value){
     $pwa.getSWRegistration().pushManager.getSubscription().then( async (subscription) =>{
       await subscription.unsubscribe() 
       // This only unsubscribes locally, but deleting it on the server is not important since it will autodelete once the server tries to send it and realizes its deleted client side
       // It will throw an error in console, but that is normal and expected (since the subscription is gone).
       await checkPushSubscription()
-      return
+      console.log("Unsubscribed from notifications")
     })
+    return
   }
-  if ("serviceWorker" in navigator && "Notification" in window && session?.value?.data?.user?.id) {
+  else if ("serviceWorker" in navigator && "Notification" in window && session?.value?.data?.user?.id) {
     Notification.requestPermission()
       .then(async (permission) => {
         console.log("Permission:", permission);
