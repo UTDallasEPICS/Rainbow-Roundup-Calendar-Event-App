ecc56ca60762ed75bad42bda18208678d7008e7e
Vikas Thoutam <vikas.thoutam@gmail.com>
2026-05-11 23:46:50 -0500

    fix: update profile picture upload and fetch

---
+++ server/api/user/[id]/index.get.ts
@@ -0,0 +1,107 @@
+/*
+There is an issue in how user: singleUser is being returned and what [id].vue is picking up.
+Cannot log in properly, profile page is not working, and data is not being populated for user.
+SEEMS TO BE FIXED: 6/28
+*/
+
+import { auth } from "~/server/auth"
+import type { User } from "../../../types/session";
+
+export default defineEventHandler(async (event) => {
+  const prisma = event.context.prisma;
+  const id = getRouterParam(event, "id");
+  const session = await auth.api.getSession({
+      headers:  event.headers
+  })
+  const currentUser = session?.user as User | undefined;
+  // DEBUGGING
+  // console.log('debug: ', currentUser)
+  // console.log('trying to access id data')
+
+  try {
+    let singleUser;
+    if (id) {
+      // make sure user that tries to access is authenticated and has a role
+      if (!currentUser) {
+        setResponseStatus(event, 401);
+        return {
+          success: false,
+          error: "Not authenticated.",
+        };
+      }
+      // users with privileges have access to all data displayed
+      if (id === currentUser.id) {
+        // Allow users to view their own full profile
+        singleUser = await prisma.user.findUnique({
+          where: { id },
+          select: {
+            id: true,
+            firstname: true,
+            lastname: true,
+            email: true,
+            phoneNum: true,
+            role: true,
+            profilePic: true,
+            SignUps: true,
+            emailNotif: true,
+            nativeNotif:true,
+          },
+        });
+      } else if (currentUser.role === "ADMIN" || currentUser.role === "SUPER") {
+        // Allow admin/super to view full profile of others
+        singleUser = await prisma.user.findUnique({
+          where: { id },
+          include: {
+            CreatedEvents: true,
+            SignUps: true,
+            Reports: true,
+            PotentialOffenses: true,
+          },
+        });
+      } else {
+        // Basic public fields only
+        singleUser = await prisma.user.findUnique({
+          where: { 
+            id,
+            isBanned: false,
+            isArchived: false
+           },
+          select: {
+            id: true,
+            firstname: true,
+            lastname: true,
+            profilePic: true,
+            SignUps: true,
+          },
+        });
+      }
+
+      if (!singleUser) {
+        setResponseStatus(event, 404);
+        return {
+          success: false,
+          error: `No user found with ID: ${id}`,
+        };
+      }
+      setResponseStatus(event, 200);
+      return {
+        success: true,
+        user: singleUser,
+      };
+    } else {
+      setResponseStatus(event, 400);
+      return {
+        success: false,
+        error: "Include an ID in your query next time, dipshit.",
+      };
+    }
+  } catch (error) {
+    setResponseStatus(event, 500);
+    const errorMessage =
+      error instanceof Error ? error.message : "Unknown error occurred";
+    return {
+      success: false,
+      error: `Error fetching user(s): ${errorMessage}`,
+    };
+  }
+});
---
+++ server/api/user/[id]/index.put.ts
@@ -0,0 +1,81 @@
+
+import { auth } from "~/server/auth"
+import type { User } from "../../../types/session";
+
+export default defineEventHandler(async (event) => {
+  const prisma = event.context.prisma;
+  // Debugging: Log the params to verify the URL structure
+  const id = getRouterParam(event, 'id')
+
+  if (!id) {
+    setResponseStatus(event, 400)
+    return {
+      success: false,
+      error: 'User ID is required to update the user.',
+    };
+  }
+
+  const body = await readBody(event);
+
+  try {
+    const session = await auth.api.getSession({
+          headers:  event.headers
+    })
+    const user = session?.user as User | undefined;
+
+    if (!user || (!["SUPER", "ADMIN"].includes(user.role) && user.id !== id)) {
+      throw createError({
+        statusCode: 403,
+        statusMessage: "Unauthenticated",
+      });
+    }
+
+    // Fetch the existing user before updating
+    const existingUser = await prisma.user.findUnique({
+      where: { id },
+    });
+
+    if (!existingUser) {
+      setResponseStatus(event, 404)
+      return {
+        success: false,
+        error: `User with ID ${id} not found.`,
+      };
+    }
+
+    const updateData: any = {};
+    if(body.email) 
+      updateData.email = body.email
+    if(body.firstname) 
+      updateData.firstname = body.firstname
+    if(body.lastname) 
+      updateData.lastname = body.lastname
+    if(body.phoneNum) 
+      updateData.phoneNum = body.phoneNum
+    if(body.profilePic)
+      updateData.profilePic = body.profilePic
+    if(body.emailNotif != null) // This is a boolean, so a plain if statement won't work
+      updateData.emailNotif = body.emailNotif
+      if(body.nativeNotif != null) // This is a boolean, so a plain if statement won't work
+      updateData.nativeNotif = body.nativeNotif
+    if (body.role) updateData.role = body.role;
+    if (body.isBanned != null) {updateData.isBanned = body.isBanned}
+    // Perform the update
+    const updatedUser = await prisma.user.update({
+      where: { id },
+      data: updateData
+    });
+    setResponseStatus(event, 200)
+    return {
+      success: true,
+      user: updatedUser,
+    };
+  } catch (error) {
+    setResponseStatus(event, 500)
+    const errorMessage = error instanceof Error ? error.message : 'Unknown error occurred';
+    return {
+      success: false,
+      error: `Error updating user: ${errorMessage}`,
+    };
+  }
+});
